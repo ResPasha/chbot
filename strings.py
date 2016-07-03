14dc44baee08d997c77749f1b2f02e579d0f0a9c
respasha <respasha@yandex.ru>
2016-07-03 08:40:05 +0300

    Strings expanded

--- strings.py
+++ strings.py
@@ -1,26 +1,38 @@
+misc_source_link = 'https://github.com/p-hash/chbot'
+
 cmd_start = 'start'
 cmd_users = 'users'
 cmd_user = 'user'
+cmd_error = 'error'
 
 msg_first_name = 'First name: '
 msg_last_name = 'Last name: '
 msg_username = 'Username: @'
 msg_users = 'Users list:'
-msg_start = '''Hello there!
+msg_reply_sent = 'Reply sent!'
+
+msg_intro = '''
+Hello there!
 I will forward all your messages to my creator.
 Feel free to ask anything
 '''
-msg_reply_sent = 'Reply sent!'
-msg_intro = 'Hello there!'
-msg_help = 'Little help'
-msg_info = 'Source code, author and contact info'
+msg_help = '''
+This bot was created for handling feedback.
+Keep in mind, than all messages you send to this bot are stored. The creator can access them and doesn't provide any warranty, privacy policy or any other statements you can trust in.
+That means that all your messages could be forwarded to any other user, group or channel without your permission. It doesn't depend on you really.
+'''
+msg_info = '''
+This bot is created by @phash using Python and MongoDB.
+You can find source code here:
+''' + misc_source_link
 
-errmsg_unknown_cmd = 'Unknown command.'
+errmsg_unknown_cmd = '''Unknown command.
+Just send me plain text message and I will forward it to my creator.
+'''
 errmsg_err = 'An error occurred:'
-errmsg_no_control = 'No control constructor with such name'
 
 ca_done = 'Done!'
 
 btn_intro = 'Intro'
 btn_help = 'Help'
-btn_info = 'Info'
+btn_info = 'About'
